775edd6ee388ad8a0256aa3f50f0ecbc428516d1
Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
2019-10-10 22:03:44 +0200

    Add dispatcher template

---
+++ src/main/java/org/eqasim/auckland_example/AucklandDispatcherModule.java
@@ -0,0 +1,15 @@
+package org.eqasim.auckland_example;
+
+import org.eqasim.auckland_example.my_dispatcher.MyDispatcher;
+import org.matsim.core.controler.AbstractModule;
+
+import ch.ethz.matsim.av.framework.AVUtils;
+
+public class AucklandDispatcherModule extends AbstractModule {
+
+	@Override
+	public void install() {
+		AVUtils.registerDispatcherFactory(binder(), "MyDispatcher", MyDispatcher.Factory.class);
+	}
+
+}
---
+++ src/main/java/org/eqasim/auckland_example/my_dispatcher/MyDispatcher.java
@@ -0,0 +1,48 @@
+package org.eqasim.auckland_example.my_dispatcher;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.router.util.TravelTime;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+import ch.ethz.idsc.amodeus.dispatcher.core.RebalancingDispatcher;
+import ch.ethz.idsc.amodeus.net.MatsimAmodeusDatabase;
+import ch.ethz.matsim.av.config.operator.OperatorConfig;
+import ch.ethz.matsim.av.dispatcher.AVDispatcher;
+import ch.ethz.matsim.av.framework.AVModule;
+import ch.ethz.matsim.av.router.AVRouter;
+
+public class MyDispatcher extends RebalancingDispatcher {
+	public MyDispatcher(Config config, OperatorConfig operatorConfig, TravelTime travelTime, AVRouter router,
+			EventsManager eventsManager, MatsimAmodeusDatabase db) {
+		super(config, operatorConfig, travelTime, router, eventsManager, db);
+	}
+
+	@Override
+	protected void redispatch(double now) {
+
+	}
+
+	public static class Factory implements AVDispatcherFactory {
+		@Inject
+		@Named(AVModule.AV_MODE)
+		private TravelTime travelTime;
+
+		@Inject
+		private EventsManager eventsManager;
+
+		@Inject
+		private Config config;
+
+		@Inject
+		private MatsimAmodeusDatabase db;
+
+		@Override
+		public AVDispatcher createDispatcher(OperatorConfig operatorConfig, AVRouter router, Network network) {
+			return new MyDispatcher(config, operatorConfig, travelTime, router, eventsManager, db);
+		}
+	}
+}
